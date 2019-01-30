8ad48e8f6891bb41b20853bb40ccf1d55e0c9ed6
Unknown <maartp@hotmail.com>
2019-01-30 14:37:16 +0100

    [Added] B4_V2 and B5, Fixed B6 and refactored while in main

--- Microcontrollers2.3/main.c
+++ Microcontrollers2.3/main.c
@@ -12,6 +12,9 @@
 void OpdrachtB2();
 void OpdrachtB3();
 void OpdrachtB4();
+void OpdrachtB4_V2();
+void OpdrachtB5();
+void OpdrachtB6();
 
 /******************************************************************/
 void wait( int ms )
@@ -41,23 +44,22 @@ notes:			Looping forever, flipping bits on PORTD
 Version :    	DMK, Initial code
 *******************************************************************/
 {
-	//OpdrachtB2();
-	//OpdrachtB3();	
-	OpdrachtB4();
+	while(1){
+		OpdrachtB6();
+	}
 	return 1;
+	
 }
 
 void OpdrachtB2()
 {
 	DDRD = 0b11111111;			// All pins PORTD are set to output
 	
-	while (1)
-	{
-		PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
-		wait(500);				// Wait 500miliseconds
-		PORTD = 0b00100000;		// Set all leds to 0 except LED of port 6
-		wait(500);				// Wait 500miliseconds
-	}
+	PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
+	wait(500);				// Wait 500miliseconds
+	PORTD = 0b00100000;		// Set all leds to 0 except LED of port 6
+	wait(500);				// Wait 500miliseconds
+	
 }
 
 void OpdrachtB3()
@@ -65,33 +67,78 @@ void OpdrachtB3()
 	DDRD = 0b11111111;			// All pins PORTD are set to output
 	DDRC = 0b00000000;			// All pins PORTC are set to input
 	
-	while(1)
-	{
-		if(PINC & (1<< PINC0)){
-			PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
-			wait(500);				// Wait 500 miliseconds
-			PORTD = 0b00000000;		// Set all leds to 0
-			wait(500);				// Wait 500 miliseconds
-		}
+	
+	if(PINC & (1<< PINC0)){
+		PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
+		wait(500);				// Wait 500 miliseconds
+		PORTD = 0b00000000;		// Set all leds to 0
+		wait(500);				// Wait 500 miliseconds
 	}
+	
 }
 
 // Lichtloop van 8 leds dmv shiften 1
 void OpdrachtB4()
 {
 	DDRD = 0b11111111;
-	int value = 0b0000001;
+	static int value = 0b0000001;
 
-	while(1)
-	{
-		PORTD = value;
-		value = value << 1;
+	PORTD = value;
+	value = value << 1;
 		
-		if(value == 0b100000000){
-			value = 0b0000001;	
-		}
-		wait(250);
+	if(value == 0b100000000){
+		value = 0b0000001;	
 	}
+	wait(250);
+	
+}
+
+
+void OpdrachtB4_V2(){
+	DDRA = 0b11111111;
+	static int value = 0b00000001;
+	static int turn = 1;
+
+	PORTA = value;
+	if(turn)
+		value = (value << 1);
+	else
+		value = (value >> 1);
+	if(value == 0b10000000)
+		turn = 0;
+	else if(value == 0b00000001)
+		turn = 1;
+	wait(250);
+	
+}
+
+//void OpdrachtB.5 Licht effect
+typedef struct {
+	unsigned char data;
+	unsigned int delay ;
+} LIGHTEFFECT_STRUCT;
+
+LIGHTEFFECT_STRUCT lighteffect[] = {
+	{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
+	{0x00, 100},
+	{0xAA,  50}, {0x55,  50},
+	{0xAA,  50}, {0x55,  50},
+	{0xAA,  50}, {0x55,  50},
+	{0x00, 100},
+	{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
+	{0x00, 0x00}
+};
+
+void OpdrachtB5(){
+	DDRA = 0b11111111;
+	
+	int index = 0;
+	while(lighteffect[index].delay != 0){
+		PORTA = lighteffect[index].data;
+		wait(lighteffect[index].delay);
+		index++;
+	}
+	
 }
 
 void OpdrachtB6()
@@ -99,13 +146,12 @@ void OpdrachtB6()
 	DDRD = 0b11111111;			// All pins PORTD are set to output
 	DDRC = 0b00000000;			// All pins PORTC are set to input
 	
-	int PRESSED = 0;
-	int MILLISECONDS = 500;
-	
-	while(1) 
-	{
-		if(PINC & (1<< PINC0))			// Checks if port C0 is pressed = 0;
-		{
+	static int PRESSED, prev_state = 0;
+	static int MILLISECONDS = 500;
+
+	const int pushed = PINC & (1 << PINC0);
+	if(pushed && prev_state == 0)			// Checks if port C0 is pressed = 0;
+	{		
 			if(PRESSED == 0)			// If value of pressed was 0
 			{
 				PRESSED = 1;			// Used as a sort of boolean
@@ -115,12 +161,14 @@ void OpdrachtB6()
 				PRESSED = 0;			//Set PRESSED back to 0
 				MILLISECONDS = 500;		//Set time to wait back to 500 miliseconds
 			}
-		}
-		
-		PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
-		wait(MILLISECONDS);		// Wait x miliseconds
-		PORTD = 0b00000000;		// Set all leds to 0
-		wait(MILLISECONDS);		// Wait x miliseconds
-		
+		prev_state = 1;
 	}
+	else if(!pushed)		
+		prev_state = 0;
+		
+	PORTD = 0b01000000;		// Set all leds to 0 except LED of port 7
+	wait(MILLISECONDS);		// Wait x miliseconds
+	PORTD = 0b00000000;		// Set all leds to 0
+	wait(MILLISECONDS);		// Wait x miliseconds
+	
 }
