639341598d1a01b42e2001d8174583733143484c
Unknown <maartp@hotmail.com>
2019-01-30 10:56:54 +0100

    [Added] Initial Atmel Project

---
+++ Microcontrollers2.3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Microcontrollers2.3.c
+ *
+ * Created: 30-1-2019 10:55:25
+ * Author : Maarten
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
