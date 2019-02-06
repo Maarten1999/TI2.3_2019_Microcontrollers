4daf657911685db2d8657495686517a3e7c014c6
Unknown <maartp@hotmail.com>
2019-02-06 12:15:54 +0100

    [Added] Initial project B3 and B4

---
+++ B3_Segment_Display/main.c
@@ -0,0 +1,18 @@
+/*
+ * B3_Segment_Display.c
+ *
+ * Created: 6-2-2019 12:14:55
+ * Author : Maarten
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ B4_Segment_Display_Fun/main.c
@@ -0,0 +1,18 @@
+/*
+ * B4_Segment_Display_Fun.c
+ *
+ * Created: 6-2-2019 12:15:22
+ * Author : Maarten
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
