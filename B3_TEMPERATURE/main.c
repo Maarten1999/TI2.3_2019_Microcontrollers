c319c9c6aec6b160f4009036cbc51f16cf0afedc
Gerben van Dooren <pedooren@avans.nl>
2019-02-20 13:02:34 +0100

    [Changed] Code clean for B3 week 4

--- B3_TEMPERATURE/main.c
+++ B3_TEMPERATURE/main.c
@@ -8,6 +8,7 @@
 #define F_CPU 8e6
 #define LCD_E	3
 #define LCD_RS	2
+#include "lcd.h"
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
@@ -34,83 +35,6 @@ void adcInit(void)
 	// 4 en 3 interrupt disabled
 	// 2 en 1 en 0 prescaler is 64: 110
 	ADCSRA = 0b11100110;
-}
-
-
-
-void lcd_strobe_lcd(void)
-{
-	PORTC |= (1<<LCD_E);
-	_delay_ms(1);
-	PORTC &= ~(1<<LCD_E);  	// E low
-	_delay_ms(1);
-}
-
-void init(void)
-{
-	DDRC = 0xFF;
-	
-	PORTC &= ~(1<<LCD_RS);
-	
-	//Return home
-	PORTC = 0x00;
-	lcd_strobe_lcd();
-	PORTC = 0x20;
-	lcd_strobe_lcd();
-	
-	//Set 4 bits mode:
-	PORTC = 0x20;
-	lcd_strobe_lcd();
-	PORTC = 0x80;
-	lcd_strobe_lcd();
-	
-	//Display: on, cursor off, blinking  0x0c
-	PORTC = 0x00;
-	lcd_strobe_lcd();
-	PORTC = 0xC0;
-	lcd_strobe_lcd();
-	
-	//entry modeL cursor to right, no shift 0x06
-	PORTC = 0x00;
-	lcd_strobe_lcd();
-	PORTC = 0x60;
-	lcd_strobe_lcd();
-	
-	// RAM adress : 0, first position, line 1
-	PORTC = 0x80;
-	lcd_strobe_lcd();
-	PORTC = 0x00;
-	lcd_strobe_lcd();
-}
-
-void clear_display(){
-	PORTC &= ~(1<<LCD_RS); //set RS to instruction mode
-	PORTC = 0x00;
-	lcd_strobe_lcd();
-
-	// Second nibble
-	PORTC = 0x10;
-	PORTC &= ~(1<<LCD_RS); //set RS to instruction mode
-	lcd_strobe_lcd();
-}
-
-void lcd_write_data(unsigned char byte){
-	// First nibble.
-	PORTC |= (1<<LCD_RS); //set RS to character mode
-	PORTC = byte;
-	lcd_strobe_lcd();
-
-	// Second nibble
-	PORTC = (byte<<4);
-	PORTC |= (1<<LCD_RS);
-	lcd_strobe_lcd();
-}
-
-void lcd_write_string(char *string){
-	for (;*string;string++)
-	{
-		lcd_write_data(*string);
-	}
 }
 
 int main(void)
