3debb97cd370a4ba738e97c41b22485fe3a8e06d
Ralph <rasrouwe@avans.nl>
2019-02-20 11:53:43 +0100

    Added week 4 B1

--- B1_ADC_LED/main.c
+++ B1_ADC_LED/main.c
@@ -5,14 +5,46 @@
  * Author : Maarten
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
 
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+void adcInit(void)
+{
+
+    // 7 en 6 bits AREF = VCC : 01
+    // 5 ADLAR enabled : 1
+    // 4 3 2 1 0 channels alleen 0 enabled
+    ADMUX = 0b01100001;
+
+    // 6 en 7  bits enable and start hoog
+    // 5 bit free running aan
+    // 4 en 3 interrupt disabled
+    // 2 en 1 en 0 prescaler is 64: 110
+    ADCSRA = 0b11100110;
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    //PORTF op output
+    DDRF = 0x00;
+    // DDRA en DDRB op input 
+    DDRA = 0xFF;
+    DDRB = 0xFF;
+    adcInit();
+    
     while (1) 
     {
+        PORTB = ADCL;
+        PORTA = ADCH;
+        wait(100);
     }
 }
-
