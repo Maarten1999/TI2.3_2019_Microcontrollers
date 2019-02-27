f39ae8262a8f0721cc2b0efe2e7fb66e0c4295a6
Unknown <maartp@hotmail.com>
2019-02-27 10:41:25 +0100

    [Added] Initial projects week 5

---
+++ C_4Digit/main.c
@@ -0,0 +1,18 @@
+/*
+ * C_4Digit.c
+ *
+ * Created: 27-2-2019 10:40:12
+ * Author : Maarten
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ C_SPI/main.c
@@ -0,0 +1,18 @@
+/*
+ * C_SPI.c
+ *
+ * Created: 27-2-2019 10:33:34
+ * Author : Maarten
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
