d5849f6edcc883297dd1c6d8eb20b4a6cb65ec3e
Ralph <rasrouwe@avans.nl>
2019-03-20 09:42:27 +0100

    init eindopdracht project

---
+++ Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 20-3-2019 09:41:59
+ * Author : Ralph Rouwen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
